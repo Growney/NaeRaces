96a92787a31a74fad52b9569f82742a43b52cbb5
Graeme Rowney <growney@tensor.co.uk>
2026-06-06 18:03:02 +0100

    Use blazor bootstrap

--- NaeRaces.BlazorWebApp/Program.cs
+++ NaeRaces.BlazorWebApp/Program.cs
@@ -2,10 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NaeRaces.BlazorWebApp;
 using NaeRaces.BlazorWebApp.Extensions;
+using NaeRaces.BlazorWebApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazorBootstrap();
+builder.Services.AddScoped<ClubContext>();
 
 await builder.AddApiClients();
 
