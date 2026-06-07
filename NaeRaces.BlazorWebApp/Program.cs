76a5a2b2829ac80cdc634e4bca412169c7e8cdb9
Graeme Rowney <growney@tensor.co.uk>
2026-06-07 18:45:07 +0100

    Using mud blazor

--- NaeRaces.BlazorWebApp/Program.cs
+++ NaeRaces.BlazorWebApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using NaeRaces.BlazorWebApp;
 using NaeRaces.BlazorWebApp.Extensions;
 using NaeRaces.BlazorWebApp.Services;
@@ -8,8 +9,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddBlazorBootstrap();
-
 await builder.AddApiClients();
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
